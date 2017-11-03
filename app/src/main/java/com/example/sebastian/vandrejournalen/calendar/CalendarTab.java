1d0631f69851766c6f4edf2bba5b16b9c9c0f3cf
Sebastian <sebqa3009@gmail.com>
2017-11-03 14:50:58 +0100

    Aftaler i kalender

--- app/src/main/java/com/example/sebastian/vandrejournalen/calendar/CalendarTab.java
+++ app/src/main/java/com/example/sebastian/vandrejournalen/calendar/CalendarTab.java
@@ -27,44 +27,44 @@ public class CalendarTab extends Fragment {
     public CalendarView calendarView;
     ArrayList<CalendarEvent> arrayList = new ArrayList<CalendarEvent>();
     Schedule calendar = new Schedule();
-
-    private static final DateFormat FORMATTER = SimpleDateFormat.getDateInstance();
     private CalendarTab.OnFragmentInteractionListener mListener;
-
 
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_calendartab,container,false);
+        final View rootView = inflater.inflate(R.layout.fragment_calendartab,container,false);
         setHasOptionsMenu(true);
         calendarView =  rootView.findViewById(R.id.calendarView);
 
-        calendar.getRecentEvents();
+        setUpCalendar();
+        return rootView;
 
-        for (int i=0;i<arrayList.size();i++){
-            arrayList.get(i).getDate();
+    }
+
+    private void setUpCalendar() {
+        arrayList = calendar.getAllEvents();
+
+        for(int i=0; i<arrayList.size();i++){
+            calendarView.addEvent(arrayList.get(i).getDay(),arrayList.get(i).getMonth(),arrayList.get(i).getYear());
         }
-
-
-        calendarView.addEvent(3,11,2017);
-
-        calendarView.addEvent(6,11,2017);
-        calendarView.addEvent(9,11,2017);
 
         calendarView.setOnDayClickListener(new CalendarView.OnDayClickListener() {
             @Override
             public void onClick(int day, int month, int year, boolean hasEvent) {
                 //Toast.makeText(getActivity(), day+"/"+month+"/"+year + " hasEvent="+hasEvent, Toast.LENGTH_SHORT).show();
                 if (hasEvent) {
-                    mListener.showPreview(new CalendarEvent(""+day+"/"+month+"/"+year,"FEEEEST"));
+                    for (int i=0; i< arrayList.size();i++){
+                        if(day == arrayList.get(i).getDay() && month == arrayList.get(i).getMonth() && year == arrayList.get(i).getYear()){
+                            mListener.showPreview(arrayList.get(i));
+
+                        }
+                    }
                 } else{
                     mListener.removePreview();
                 }
             }
         });
-        return rootView;
-
     }
 
     @Override
